8b6c3921f37826cd91f7a2f115b99955e72b8303
NINESTAIRS <jhjzzang14@gmail.com>
2017-07-28 13:19:27 +0900

    remove hybrid-test

--- hybrid-test/nodeApi/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-
-var Login = require('./api/Login')(app);
-
-var server = app.listen(3000,function(){
-    console.log('server start');
-});
